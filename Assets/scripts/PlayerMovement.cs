8798199210e3156b0b96509716376f68041905f0
DawidWejmanATU <145448613+DawidWejmanATU@users.noreply.github.com>
2024-03-14 15:02:18 +0000

    PlayerMovement Completed

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{ 
+    [SerializeField] private float speed;
     private Rigidbody2D body;
-
-    private void await()
+ 
+    private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
+ 
     private void Update()
     {
-        body.velocity = new Vector3(Input.GetAxis("Horizontal"), body.velocity.z);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+ 
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
-
 }
